e50ffbd4d5949eeff2e4a109b2a63ace00b7aaa2
msyk <kouno@msyk.works>
2022-06-06 17:00:12 +0900

    Enable NSPersistentStoreRemoteChangeNotificationPost

--- Sources/CoreDataX/StoreDescriptions/CloudKitPersistentStoreDescription.swift
+++ Sources/CoreDataX/StoreDescriptions/CloudKitPersistentStoreDescription.swift
@@ -8,6 +8,7 @@ public final class CloudKitPersistentStoreDescription: SQLitePersistentStoreDesc
     public init(url: URL, containerIdentifier: String) {
         super.init(url: url)
         rawValue.cloudKitContainerOptions = .init(containerIdentifier: containerIdentifier)
+        rawValue.setOption(true as NSNumber, forKey: NSPersistentStoreRemoteChangeNotificationPostOptionKey)
     }
 
     public convenience init(containerIdentifier: String, fileName: String, groupName: String? = nil) {
